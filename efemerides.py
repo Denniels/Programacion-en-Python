6ad1861ca421d85e1ddc6e7d2bbd2c116347c05c
Denniels <34431737+Denniels@users.noreply.github.com>
2022-07-02 00:16:31 -0400

    Update efemerides.py

--- efemerides.py
+++ efemerides.py
@@ -1,3 +1,4 @@
+# Creamos diccionario con las fechas de las efemerides
 efemerides = {'1 de enero':'Año nuevo',
               '27 de febrero':'Terremoto en chile',
               '8 de marzo':'Dia de la Mujer',
@@ -6,6 +7,12 @@ efemerides = {'1 de enero':'Año nuevo',
               '19 de septiembre':'Glorias del Ejercito',
               '25 de diciembre':'Navidad'}
 
+'''
+Definición de variable y consultas a usuario,
+usamos la funcion lower para evitar errores de tipeo,
+asi buscamos solo letras minusculas
+'''
 fecha = input("Ingrese una fecha: ").lower()
 
+# Salida, y si no hay coincidencias imprime un mensaje
 print(f'Efemerides: {efemerides.get(fecha, "No hay eventos para esta fecha")}')
