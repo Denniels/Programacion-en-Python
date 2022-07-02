67196f171693d8a77bee133b1b56ba1c8c6fc65a
Denniels <34431737+Denniels@users.noreply.github.com>
2022-07-02 00:06:31 -0400

    Update calorias.py

--- calorias.py
+++ calorias.py
@@ -1,9 +1,13 @@
+# Importamos la libreria maath que contiene la funcion ceil.()
 import math
 
+# Definición de variables y consultas a usuario
 proteina = float(input("Ingrese los gr de protenias: \n"))
 carbohidratos = float(input("Ingrese los gr de csrbohidratos \n"))
 grasa = float(input(" Ingrese los gr de grasas \n"))
 
+# Proceso
 calorias = 4 * (proteina + carbohidratos) + 9 * grasa
 
+# Salida
 print(f'Las calorias totales del producto son: {math.ceil(calorias)}')
