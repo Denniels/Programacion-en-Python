aded72cf74d2e60238ca0e89bd52b9365bca880d
Denniels <34431737+Denniels@users.noreply.github.com>
2022-07-14 19:56:15 -0400

    Cambios

---
+++ Scrpts/ciclos_c3.py
@@ -0,0 +1,7 @@
+number = int(input('Registrar un numero '))
+
+while number > 1 and number < 10:
+    number = int(input('Registrar un nuevo numero '))
+    print ('El numero ingresado esta entre el 1 y el 10')
+    
+print('El numero no esta dentro del 1 y el 10')
---
+++ Scrpts/counter.py
@@ -0,0 +1,9 @@
+#import time
+iterador = 0
+
+while iterador < 10:
+    iterador += 1
+    #time.sleep(1)
+    print('El valor de lña variable actualmente despues del loop es: ', iterador)
+
+print('El ciclo a terminado')
