7999a038668a85de5452632e515976e614354268
Denniels <34431737+Denniels@users.noreply.github.com>
2022-07-14 22:13:59 -0400

    Update primeros_auxilios.py

--- Scrpts/primeros_auxilios.py
+++ Scrpts/primeros_auxilios.py
@@ -1,28 +1,32 @@
+ambulancia=('no')
+estimulos = input("¿Responde a Estimulos? [Si/No]: ").lower()
 
-estimulo = input('Responde a estimulos ').lower()
-if estimulo == str("si"):
-    print('Llevarlo al hospital mas cercano')
+# if (estimulos!='si' or estimulos!='no'):
+#     print('Debe ingresar solo Si o No..')
 
+if (estimulos=='si'):
+    print ('Valorar la necesidad de llevarlo al hospital mas cercano')
+    #print ('Fin')
 else:
-    print('Abrir via aerea')
-    respira = input('Respira ').lower()
-    if respira == str('si'):
-        print('Permitir posicion de suficiente respiracion')
+    print ('Abrir la via Aérea')
+    respira = input("¿Respira? [Si/No]: ").lower()
+    # if (respira!='si' or respira!='no'):
+    #     print('Debe ingresar solo Si o No..')
+    if (respira=='si'):
+        print ('Permitirle posición de suficiente ventilación')
+        #print ('Fin')
     else:
-
-        
-        print('Administrar 5 ventilaciones y llamar a una ambulancia ')
-        vida = input('¿Signos de vida? ').lower()
-        if vida == str('si'):
-            print('Reevaluar la espera de la ambulancia')
-            ambulancia = input('Llego la ambulancia ')
-
-        
-
-
-        else:
-            print('Administrar compreciones toracicas hasta que llegue la ambulancia')
-            
+        print ('Administrar 5 Ventilaciones y llamar a Ambulancia')
+    
+        while (ambulancia=='no'):
+            signos = input("¿Signos de Vida? [Si/No]: ").lower()
+            if (signos=='si'):
+                print ('Reevaluar a la espera de la Ambulancia')           
+                ambulancia = input("¿Llegó la Ambulancia? [Si/No]: ").lower()
+            else:
+                print ('Administrar Compresiones Torácicas hasta que llegue ambulancia')
+                ambulancia = input("¿Llegó la Ambulancia? [Si/No]: ").lower()
+print ('Fin')
 
 
     
