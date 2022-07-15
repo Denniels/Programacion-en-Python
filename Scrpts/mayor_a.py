d15f0262b808b1544cca2e2a3dd88011b42c6cd0
Denniels <34431737+Denniels@users.noreply.github.com>
2022-07-15 13:14:09 -0400

    Cambios

--- Scrpts/mayor_a.py
+++ Scrpts/mayor_a.py
@@ -1,6 +1,17 @@
+'''
+Se solicita devolver un informe resumido que exponga los meses que superan un cierto
+umbral. El programa mayor_a.py debe retornar un diccionario con el mes y el valor
+asociado siempre y cuando superen el umbral especificado.
+Ejemplo:
+python mayor_a.py 40000
+{'Mayo': 81000, 'Agosto': 41200, 'Noviembre': 91000}
+'''
+
 from sys import argv
 
 consulta = int(argv[1])
+
+resultado = {}
 
 ventas = {"Enero": 15000,
           "Febrero": 22000,
@@ -15,4 +26,15 @@ ventas = {"Enero": 15000,
           "Noviembre": 91000,
           "Diciembre": 21000,}
 
-valores = ventas.values()
+
+for mes in ventas.keys():
+    if ventas.get(mes) > consulta:
+        resultado[mes]=ventas.get(mes)
+
+print(resultado)
+
+
+
+'''for i in ventas:
+    if ventas.get(i) > consulta:
+        print(i, ventas.get(i))'''
