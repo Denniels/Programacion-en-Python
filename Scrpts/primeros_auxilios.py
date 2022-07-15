6d9e122a46a752c6896223e7ce4c74a53156fa6a
Denniels <34431737+Denniels@users.noreply.github.com>
2022-07-15 08:08:41 -0400

    Update primeros_auxilios.py

--- Scrpts/primeros_auxilios.py
+++ Scrpts/primeros_auxilios.py
@@ -1,31 +1,30 @@
-ambulancia=('no')
-estimulos = input("¿Responde a Estimulos? [Si/No]: ").lower()
+estimulos = input("¿Responde a estimulos?: ").lower()
 
-# if (estimulos!='si' or estimulos!='no'):
-#     print('Debe ingresar solo Si o No..')
-
-if (estimulos=='si'):
+if estimulos == 'si':
     print ('Valorar la necesidad de llevarlo al hospital mas cercano')
-    #print ('Fin')
+
 else:
     print ('Abrir la via Aérea')
-    respira = input("¿Respira? [Si/No]: ").lower()
-    # if (respira!='si' or respira!='no'):
-    #     print('Debe ingresar solo Si o No..')
-    if (respira=='si'):
+    respira = input("¿Respira?: ").lower()
+
+    if respira == 'si':
         print ('Permitirle posición de suficiente ventilación')
-        #print ('Fin')
+
     else:
         print ('Administrar 5 Ventilaciones y llamar a Ambulancia')
-    
-        while (ambulancia=='no'):
-            signos = input("¿Signos de Vida? [Si/No]: ").lower()
-            if (signos=='si'):
+        ambulancia = 'no'
+
+        while ambulancia == 'no':
+            signos = input("¿Signos de Vida?: ").lower()
+
+            if signos == 'si':
                 print ('Reevaluar a la espera de la Ambulancia')           
-                ambulancia = input("¿Llegó la Ambulancia? [Si/No]: ").lower()
+                ambulancia = input("¿Llegó la Ambulancia?: ").lower()
+
             else:
                 print ('Administrar Compresiones Torácicas hasta que llegue ambulancia')
-                ambulancia = input("¿Llegó la Ambulancia? [Si/No]: ").lower()
+                ambulancia = input("¿Llegó la Ambulancia?: ").lower()
+
 print ('Fin')
 
 
